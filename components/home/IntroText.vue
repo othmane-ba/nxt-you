162133b4786133c171d7ce476872a1e0378b6ec8
Fabian Piper <fabianpiper@web.de>
2021-04-29 13:31:02 +0200

    IntroText Refactor

--- components/home/IntroText.vue
+++ components/home/IntroText.vue
@@ -1,38 +1,67 @@
 <template>
-  <!-- HERO INNER -->
-  <div class="hero__inner">
-    <!-- CONTAINER -->
-    <div class="container">
-      <img
-        class="hero__inner__logo mb-4 mt-4"
-        src="~/assets/images/logo.png"
-        alt="NXT YOU"
-      />
-
-      <!-- HERO TITLE  -->
-      <div class="hero__inner__title mb-5 mt-5">
-        <h1 class="cd-headline clip home-headline">
-          I’m a
-          <span class="cd-words-wrapper single-headline"
-            ><b class="is-visible">Developer</b><b>Designer</b
-            ><b>Freelancer</b></span
+  <div class="container max-w-6xl mx-auto px-4">
+    <div class="grid grid-cols-1 lg:grid-cols-2 gap-4 lg:gap-y-6">
+      <div class="lg:col-span-2">
+        <h2 class="text-center uppercase text-5xl font-bold">
+          <span
+            data-animation-child
+            class="title-fill"
+            data-animation="title-fill-anim"
+            data-text="We are Creative"
+            >Make your Vision</span
+          ><br />
+          <span
+            data-animation-child
+            class="title-fill tr-delay01"
+            data-animation="title-fill-anim"
+            data-text="We are Xen"
+            >Come Reality.</span
           >
-        </h1>
+        </h2>
       </div>
-      <!-- /HERO TITLE -->
 
-      <!-- HERO BUTTON -->
-      <button
-        class="button button--outline button--rec hero__inner__button pointer-large js-down-button"
-      >
-        <i class="ti-arrow-down"></i>
-        <i class="ti-arrow-down"></i>
-      </button>
-      <!-- /HERO BUTTON -->
+      <div>
+        <p
+          data-animation-child
+          class="fade-anim-box tr-delay02"
+          data-animation="fade-anim"
+        >
+          Affogato thundercats quinoa, portland cold-pressed edison bulb artisan
+          paleo banjo tousled try-hard food truck pop-up bushwick godard. Occupy
+          90's try-hard tote bag chicharrones stumptown polaroid hashtag cliche
+          +1, tousled fanny pack. Tote bag iPhone crucifix hella helvetica food
+          truck bicycle rights cloud bread. Yr iPhone asymmetrical, next level
+          vexillologist godard blog green juice chia. Tacos jean shorts pickled
+          PBR&B poutine.
+        </p>
+      </div>
+
+      <div>
+        <p
+          data-animation-child
+          class="fade-anim-box tr-delay03"
+          data-animation="fade-anim"
+        >
+          Godard slow-carb chartreuse occupy, tumblr letterpress pok pok
+          tattooed yr lyft yuccie kinfolk. IPhone kombucha shaman gastropub
+          snackwave 90's lo-fi pug chillwave pok pok tofu. Swag deep v listicle
+          roof party seitan man braid raclette church-key trust fund locavore
+          vexillologist green juice raw denim tilde meh. Austin thundercats
+          locavore taiyaki snackwave hoodie put a bird on it tattooed selvage
+          kitsch ramps.
+        </p>
+      </div>
+
+      <div class="lg:col-span-2">
+        <strong
+          data-animation-child
+          class="text-xs text-center tracking-widest uppercase leading-4 fade-anim-box tr-delay04"
+          data-animation="fade-anim"
+          >Godard slow-carb chartreuse occupy, tumblr letterpress</strong
+        >
+      </div>
     </div>
-    <!-- /CONTAINER -->
   </div>
-  <!-- /HERO INNER -->
 </template>
 
 <script lang="ts">
