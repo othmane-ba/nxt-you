ffd4bbc11ba741441be25aafe7e6162209739e22
Fabian Piper <fabianpiper@web.de>
2021-06-04 14:59:08 +0200

    Fix smooth device detection

--- plugins/smooth-scrolling.client.ts
+++ plugins/smooth-scrolling.client.ts
@@ -1,13 +1,10 @@
 import Parallax from '~/assets/scripts/parallax'
 
-export default (
-  app: { isMobileOrTablet: any },
-  inject: (arg0: string, arg1: Parallax) => void
-) => {
+export default (app: any, inject: any) => {
   const smooth = new Parallax({
     native: true,
     section: document.querySelector('.vs-section'),
-    ease: app.isMobileOrTablet ? 1 : 0.1,
+    ease: app.$device.isMobileOrTablet ? 1 : 0.1,
     divs: document.querySelectorAll('.vs-div'),
   })
 
