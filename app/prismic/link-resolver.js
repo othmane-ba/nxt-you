08020f074d822c22166a67a895c3a4a70870a13a
Fabian Piper <fabianpiper@web.de>
2021-08-08 13:40:43 +0200

    Init prismic with sm

---
+++ app/prismic/link-resolver.js
@@ -0,0 +1,10 @@
+export default function (doc) {
+  if (doc.isBroken) {
+    return '/not-found'
+  }
+  if (doc.type === 'page') {
+    return `/${doc.uid}`
+  } else {
+    return null
+  }
+}
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,6 @@
 import pwa from './pwa.config'
-
 export default {
   target: 'static',
-
   head: {
     title: 'NXT YOU',
     titleTemplate: '%s - Make Your Vision Come Reality.',
@@ -13,8 +11,13 @@ export default {
       class: 'bg-black text-white',
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        charset: 'utf-8',
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
     ],
     link: [
       {
@@ -35,19 +38,24 @@ export default {
         href: '/favicon-16x16.png',
       },
       /*
-      { rel: 'manifest', href: '/site.webmanifest' },
-*/
+    { rel: 'manifest', href: '/site.webmanifest' },
+    */
+    ],
+    script: [
+      {
+        src: 'https://cdn.polyfill.io/v2/polyfill.min.js?features=Element.prototype.classList',
+      },
+      {
+        src: 'https://cdn.jsdelivr.net/npm/focus-visible@5.0.2/dist/focus-visible.min.js',
+      },
     ],
   },
-
   env: {
     baseUrl: process.env.BASE_URL || 'http://localhost:3000',
     googleAnalyticsId: process.env.GOOGLE_ANALYTICS_ID,
     dev: process.env.NODE_ENV !== 'production',
   },
-
-  css: [],
-
+  css: ['vue-essential-slices/src/styles/styles.scss'],
   plugins: [
     '~/plugins/vue-awesome-swiper.client.js',
     '~/plugins/vue-check-view.js',
@@ -57,11 +65,9 @@ export default {
     '~/plugins/jsonld.js',
     '~/plugins/vue-gtag.client.js',
   ],
-
   components: {
     dirs: ['~/components/home', '~/components/layout', '~/components/shared'],
   },
-
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     '@nuxtjs/tailwindcss',
@@ -71,31 +77,43 @@ export default {
     '@nuxtjs/toast',
     '@nuxtjs/pwa',
     'cookie-universal-nuxt',
+    '@nuxtjs/prismic',
   ],
-
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: ['@nuxtjs/axios', '@nuxtjs/cloudinary'],
-
+  modules: ['@nuxtjs/axios', '@nuxtjs/cloudinary', '@nuxtjs/prismic'],
   axios: {
     baseURL: process.env.BASE_URL,
-    headers: { post: { 'Content-Type': 'application/json' } },
+    headers: {
+      post: {
+        'Content-Type': 'application/json',
+      },
+    },
   },
-
   toast: {
     position: 'bottom-right',
     duration: 3000,
   },
-
   pwa,
-
   tailwindcss: {
     cssPath: '~/assets/css/tailwind.css',
   },
-
   cloudinary: {
     cloudName: process.env.CLOUDINARY_CLOUDNAME,
     useComponent: true,
   },
-
-  build: {},
+  prismic: {
+    endpoint: 'https://nxtyou.cdn.prismic.io/api/v2',
+    modern: true,
+    apiOptions: {
+      routes: [
+        {
+          type: 'page',
+          path: '/:uid',
+        },
+      ],
+    },
+  },
+  build: {
+    transpile: ['vue-slicezone', 'nuxt-sm'],
+  },
 }
