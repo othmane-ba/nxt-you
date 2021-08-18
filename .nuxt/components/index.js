81a182b9c1aad4a0523c230b1141af9ace1d9af1
Fabian Piper <fabianpiper@web.de>
2021-08-18 10:28:19 +0200

    Cleanup components

--- .nuxt/components/index.js
+++ .nuxt/components/index.js
@@ -1,53 +1,21 @@
 import { wrapFunctional } from './utils'
 
-export { default as BrandTeaser } from '../..\\components\\home\\BrandTeaser.vue'
-export { default as ClientList } from '../..\\components\\home\\ClientList.vue'
-export { default as FeatureSlider } from '../..\\components\\home\\FeatureSlider.vue'
-export { default as Hero } from '../..\\components\\home\\Hero.vue'
 export { default as HeroHeadline } from '../..\\components\\home\\HeroHeadline.vue'
-export { default as IntroText } from '../..\\components\\home\\IntroText.vue'
-export { default as MarketingSlider } from '../..\\components\\home\\MarketingSlider.vue'
-export { default as Qualifier } from '../..\\components\\home\\Qualifier.vue'
 export { default as Sphere } from '../..\\components\\home\\Sphere.vue'
-export { default as StickyVideoDisplay } from '../..\\components\\home\\StickyVideoDisplay.vue'
-export { default as TestimonialSlider } from '../..\\components\\home\\TestimonialSlider.vue'
-export { default as VideoGrid } from '../..\\components\\home\\VideoGrid.vue'
-export { default as Work } from '../..\\components\\home\\Work.vue'
 export { default as CookieHint } from '../..\\components\\layout\\CookieHint.vue'
 export { default as Footer } from '../..\\components\\layout\\Footer.vue'
 export { default as PageLoader } from '../..\\components\\layout\\PageLoader.vue'
 export { default as Pointer } from '../..\\components\\layout\\Pointer.vue'
-export { default as CaptionImage } from '../..\\components\\shared\\CaptionImage.vue'
-export { default as Contact } from '../..\\components\\shared\\Contact.vue'
-export { default as HoverImage } from '../..\\components\\shared\\HoverImage.vue'
-export { default as Intersection } from '../..\\components\\shared\\Intersection.vue'
-export { default as NumText } from '../..\\components\\shared\\NumText.vue'
 export { default as PageHeader } from '../..\\components\\shared\\PageHeader.vue'
 export { default as RangeSlider } from '../..\\components\\shared\\RangeSlider.vue'
 export { default as Tag } from '../..\\components\\shared\\Tag.vue'
 
-export const LazyBrandTeaser = import('../..\\components\\home\\BrandTeaser.vue' /* webpackChunkName: "components/brand-teaser" */).then(c => wrapFunctional(c.default || c))
-export const LazyClientList = import('../..\\components\\home\\ClientList.vue' /* webpackChunkName: "components/client-list" */).then(c => wrapFunctional(c.default || c))
-export const LazyFeatureSlider = import('../..\\components\\home\\FeatureSlider.vue' /* webpackChunkName: "components/feature-slider" */).then(c => wrapFunctional(c.default || c))
-export const LazyHero = import('../..\\components\\home\\Hero.vue' /* webpackChunkName: "components/hero" */).then(c => wrapFunctional(c.default || c))
 export const LazyHeroHeadline = import('../..\\components\\home\\HeroHeadline.vue' /* webpackChunkName: "components/hero-headline" */).then(c => wrapFunctional(c.default || c))
-export const LazyIntroText = import('../..\\components\\home\\IntroText.vue' /* webpackChunkName: "components/intro-text" */).then(c => wrapFunctional(c.default || c))
-export const LazyMarketingSlider = import('../..\\components\\home\\MarketingSlider.vue' /* webpackChunkName: "components/marketing-slider" */).then(c => wrapFunctional(c.default || c))
-export const LazyQualifier = import('../..\\components\\home\\Qualifier.vue' /* webpackChunkName: "components/qualifier" */).then(c => wrapFunctional(c.default || c))
 export const LazySphere = import('../..\\components\\home\\Sphere.vue' /* webpackChunkName: "components/sphere" */).then(c => wrapFunctional(c.default || c))
-export const LazyStickyVideoDisplay = import('../..\\components\\home\\StickyVideoDisplay.vue' /* webpackChunkName: "components/sticky-video-display" */).then(c => wrapFunctional(c.default || c))
-export const LazyTestimonialSlider = import('../..\\components\\home\\TestimonialSlider.vue' /* webpackChunkName: "components/testimonial-slider" */).then(c => wrapFunctional(c.default || c))
-export const LazyVideoGrid = import('../..\\components\\home\\VideoGrid.vue' /* webpackChunkName: "components/video-grid" */).then(c => wrapFunctional(c.default || c))
-export const LazyWork = import('../..\\components\\home\\Work.vue' /* webpackChunkName: "components/work" */).then(c => wrapFunctional(c.default || c))
 export const LazyCookieHint = import('../..\\components\\layout\\CookieHint.vue' /* webpackChunkName: "components/cookie-hint" */).then(c => wrapFunctional(c.default || c))
 export const LazyFooter = import('../..\\components\\layout\\Footer.vue' /* webpackChunkName: "components/footer" */).then(c => wrapFunctional(c.default || c))
 export const LazyPageLoader = import('../..\\components\\layout\\PageLoader.vue' /* webpackChunkName: "components/page-loader" */).then(c => wrapFunctional(c.default || c))
 export const LazyPointer = import('../..\\components\\layout\\Pointer.vue' /* webpackChunkName: "components/pointer" */).then(c => wrapFunctional(c.default || c))
-export const LazyCaptionImage = import('../..\\components\\shared\\CaptionImage.vue' /* webpackChunkName: "components/caption-image" */).then(c => wrapFunctional(c.default || c))
-export const LazyContact = import('../..\\components\\shared\\Contact.vue' /* webpackChunkName: "components/contact" */).then(c => wrapFunctional(c.default || c))
-export const LazyHoverImage = import('../..\\components\\shared\\HoverImage.vue' /* webpackChunkName: "components/hover-image" */).then(c => wrapFunctional(c.default || c))
-export const LazyIntersection = import('../..\\components\\shared\\Intersection.vue' /* webpackChunkName: "components/intersection" */).then(c => wrapFunctional(c.default || c))
-export const LazyNumText = import('../..\\components\\shared\\NumText.vue' /* webpackChunkName: "components/num-text" */).then(c => wrapFunctional(c.default || c))
 export const LazyPageHeader = import('../..\\components\\shared\\PageHeader.vue' /* webpackChunkName: "components/page-header" */).then(c => wrapFunctional(c.default || c))
 export const LazyRangeSlider = import('../..\\components\\shared\\RangeSlider.vue' /* webpackChunkName: "components/range-slider" */).then(c => wrapFunctional(c.default || c))
 export const LazyTag = import('../..\\components\\shared\\Tag.vue' /* webpackChunkName: "components/tag" */).then(c => wrapFunctional(c.default || c))
--- .nuxt/components/plugin.js
+++ .nuxt/components/plugin.js
@@ -2,28 +2,12 @@ import Vue from 'vue'
 import { wrapFunctional } from './utils'
 
 const components = {
-  BrandTeaser: () => import('../..\\components\\home\\BrandTeaser.vue' /* webpackChunkName: "components/brand-teaser" */).then(c => wrapFunctional(c.default || c)),
-  ClientList: () => import('../..\\components\\home\\ClientList.vue' /* webpackChunkName: "components/client-list" */).then(c => wrapFunctional(c.default || c)),
-  FeatureSlider: () => import('../..\\components\\home\\FeatureSlider.vue' /* webpackChunkName: "components/feature-slider" */).then(c => wrapFunctional(c.default || c)),
-  Hero: () => import('../..\\components\\home\\Hero.vue' /* webpackChunkName: "components/hero" */).then(c => wrapFunctional(c.default || c)),
   HeroHeadline: () => import('../..\\components\\home\\HeroHeadline.vue' /* webpackChunkName: "components/hero-headline" */).then(c => wrapFunctional(c.default || c)),
-  IntroText: () => import('../..\\components\\home\\IntroText.vue' /* webpackChunkName: "components/intro-text" */).then(c => wrapFunctional(c.default || c)),
-  MarketingSlider: () => import('../..\\components\\home\\MarketingSlider.vue' /* webpackChunkName: "components/marketing-slider" */).then(c => wrapFunctional(c.default || c)),
-  Qualifier: () => import('../..\\components\\home\\Qualifier.vue' /* webpackChunkName: "components/qualifier" */).then(c => wrapFunctional(c.default || c)),
   Sphere: () => import('../..\\components\\home\\Sphere.vue' /* webpackChunkName: "components/sphere" */).then(c => wrapFunctional(c.default || c)),
-  StickyVideoDisplay: () => import('../..\\components\\home\\StickyVideoDisplay.vue' /* webpackChunkName: "components/sticky-video-display" */).then(c => wrapFunctional(c.default || c)),
-  TestimonialSlider: () => import('../..\\components\\home\\TestimonialSlider.vue' /* webpackChunkName: "components/testimonial-slider" */).then(c => wrapFunctional(c.default || c)),
-  VideoGrid: () => import('../..\\components\\home\\VideoGrid.vue' /* webpackChunkName: "components/video-grid" */).then(c => wrapFunctional(c.default || c)),
-  Work: () => import('../..\\components\\home\\Work.vue' /* webpackChunkName: "components/work" */).then(c => wrapFunctional(c.default || c)),
   CookieHint: () => import('../..\\components\\layout\\CookieHint.vue' /* webpackChunkName: "components/cookie-hint" */).then(c => wrapFunctional(c.default || c)),
   Footer: () => import('../..\\components\\layout\\Footer.vue' /* webpackChunkName: "components/footer" */).then(c => wrapFunctional(c.default || c)),
   PageLoader: () => import('../..\\components\\layout\\PageLoader.vue' /* webpackChunkName: "components/page-loader" */).then(c => wrapFunctional(c.default || c)),
   Pointer: () => import('../..\\components\\layout\\Pointer.vue' /* webpackChunkName: "components/pointer" */).then(c => wrapFunctional(c.default || c)),
-  CaptionImage: () => import('../..\\components\\shared\\CaptionImage.vue' /* webpackChunkName: "components/caption-image" */).then(c => wrapFunctional(c.default || c)),
-  Contact: () => import('../..\\components\\shared\\Contact.vue' /* webpackChunkName: "components/contact" */).then(c => wrapFunctional(c.default || c)),
-  HoverImage: () => import('../..\\components\\shared\\HoverImage.vue' /* webpackChunkName: "components/hover-image" */).then(c => wrapFunctional(c.default || c)),
-  Intersection: () => import('../..\\components\\shared\\Intersection.vue' /* webpackChunkName: "components/intersection" */).then(c => wrapFunctional(c.default || c)),
-  NumText: () => import('../..\\components\\shared\\NumText.vue' /* webpackChunkName: "components/num-text" */).then(c => wrapFunctional(c.default || c)),
   PageHeader: () => import('../..\\components\\shared\\PageHeader.vue' /* webpackChunkName: "components/page-header" */).then(c => wrapFunctional(c.default || c)),
   RangeSlider: () => import('../..\\components\\shared\\RangeSlider.vue' /* webpackChunkName: "components/range-slider" */).then(c => wrapFunctional(c.default || c)),
   Tag: () => import('../..\\components\\shared\\Tag.vue' /* webpackChunkName: "components/tag" */).then(c => wrapFunctional(c.default || c))
