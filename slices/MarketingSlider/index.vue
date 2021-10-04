9a25b162c1c4a7493ca4818d0d143dd7bf669cff
Fabian Piper <fabianpiper@web.de>
2021-10-04 13:02:45 +0200

    Restyle marketing slider

--- slices/MarketingSlider/index.vue
+++ slices/MarketingSlider/index.vue
@@ -1,63 +1,84 @@
 <template>
   <section class="marketing-slider" :id="slice.primary.slug">
-    <div class="container mx-auto px-4 lg:px-32 overflow-hidden" v-animate>
-      <div
-        class="overflow-hidden w-full"
-        v-swiper="options"
-        :auto-destroy="true"
-        @ready="onReady"
-      >
-        <div class="swiper-wrapper">
-          <div
-            class="swiper-slide"
-            v-for="(item, i) in slice.items"
-            :key="`slice-item-${i}`"
-          >
-            <div class="grid lg:grid-cols-5 gap-4 items-center">
-              <div class="lg:col-span-2">
-                <h4 class="font-bold uppercase text-4xl">
-                  {{ item.title }}
-                </h4>
-
-                <div
-                  class="
-                    relative
-                    transform
-                    lg:translate-x-8
-                    z-10
-                    bg-black
-                    p-4
-                    mb-4
-                  "
-                >
-                  <prismic-rich-text :field="item.description" />
-                </div>
-
-                <div class="relative bg-gray-400 mr-auto mb-4 w-24 h-px">
-                  <div
-                    data-gsap-target="marketingSlider"
-                    class="absolute bg-white top-0 left-0 h-full w-0"
-                  ></div>
-                </div>
-              </div>
-              <div class="lg:col-span-3">
+    <div class="container mx-auto lg:px-32" v-animate>
+      <div class="relative space-y-4">
+        <div
+          v-simple-parallax
+          v-swiper="options"
+          :auto-destroy="true"
+          @ready="onReady"
+        >
+          <div class="swiper-wrapper">
+            <div
+              class="swiper-slide"
+              v-for="(item, i) in slice.items"
+              :key="`slice-item-${i}`"
+            >
+              <div class="relative">
+                <div class="w-full aspect-h-4 aspect-w-7"></div>
                 <img
                   v-if="item.image"
                   v-lazy
-                  class="w-full"
+                  class="absolute top-0 left-0 w-full h-full object-contain"
                   :data-src="item.image.url"
                   :alt="item.image.alt"
                 />
 
                 <CnyVideo
+                  ref="video"
                   v-if="item.video"
                   :publicId="item.video"
-                  class="relative object-cover"
+                  class="absolute top-0 left-0 w-full h-full object-contain"
                   muted
                   loop
                   autoplay
                 ></CnyVideo>
               </div>
+            </div>
+          </div>
+        </div>
+
+        <div
+          class="
+            lg:absolute lg:bottom-0
+            flex flex-col
+            lg:flex-row
+            items-start
+            lg:items-end
+            w-full
+            z-10
+          "
+        >
+          <div
+            class="max-w-2xl flex-shrink-1"
+            v-swiper="options"
+            :auto-destroy="true"
+          >
+            <div class="swiper-wrapper">
+              <div
+                class="swiper-slide"
+                v-for="(item, i) in slice.items"
+                :key="`description-slice-item-${i}`"
+              >
+                <div class="bg-black p-4 lg:py-8 space-y-4">
+                  <h4 class="font-bold text-xl">
+                    {{ item.title }}
+                  </h4>
+
+                  <div>
+                    <prismic-rich-text :field="item.description" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="bg-black flex-1 p-4 lg:py-8">
+            <div class="relative bg-gray-400 w-32 h-0.5 ml-auto">
+              <div
+                data-gsap-target="marketingSlider"
+                class="absolute bg-white top-0 left-0 h-full w-0"
+              ></div>
             </div>
           </div>
         </div>
@@ -85,7 +106,6 @@ export default {
         allowTouchMove: false,
         loop: true,
         effect: 'fade',
-        lazy: true,
         fadeEffect: {
           crossFade: true,
         },
@@ -100,7 +120,7 @@ export default {
           '[data-gsap-target="marketingSlider"]',
           this.slice.primary.autoplaySpeed,
           { width: '0%' },
-          { width: '100%' }
+          { width: '100%', ease: 'Linear.easeNone' }
         )
         .add(() => {
           swiper.slideNext()
