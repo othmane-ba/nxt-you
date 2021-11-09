0f7902574892ee50b39fafa6761b1c8a5560e3f6
Fabian Piper <fabianpiper@web.de>
2021-11-09 11:15:59 +0100

    Add pdf fallback for mobile browsers

--- slices/PdfObject/index.vue
+++ slices/PdfObject/index.vue
@@ -1,7 +1,5 @@
 <template>
-  <div data-pointer="initial">
-    <div class="relative z-60 h-screen w-screen top-0 left-0"></div>
-  </div>
+  <div data-pointer="initial"></div>
 </template>
 
 <script>
@@ -18,7 +16,33 @@ export default {
     },
   },
   mounted() {
-    PDFObject.embed(this.slice.primary.media.url)
+    PDFObject.embed(this.slice.primary.media.url, document.body, {
+      fallbackLink:
+        "<div class='relative w-screen h-screen overflow-hidden'><div class='absolute top-1/2 left-1/2 -transform-x-1/2 -transform-y-1/2 max-h-full w-full text-center'><p>Falls der Download nicht automatisch beginnt bitte <a class='link' href='[url]'>hier klicken</a>.</p></div></div>",
+    })
+    if (!PDFObject.supportsPDFs) {
+      this.$axios
+        .get(this.slice.primary.media.url, {
+          responseType: 'blob',
+        })
+        .then((res) =>
+          this.download(
+            window.URL.createObjectURL(new Blob([res.data])),
+            this.slice.primary.media.name
+          )
+        )
+        .catch((error) => console.log(error))
+    }
+  },
+  methods: {
+    download(url, name) {
+      const link = document.createElement('a')
+      link.href = url
+      link.download = name
+      document.body.appendChild(link)
+      link.click()
+      document.body.removeChild(link)
+    },
   },
 }
 </script>
