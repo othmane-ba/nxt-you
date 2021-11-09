21156131422bfb09434ef15338001b9dbb60ca74
Fabian Piper <fabianpiper@web.de>
2021-11-09 11:26:26 +0100

    Change browser fallback

--- slices/PdfObject/index.vue
+++ slices/PdfObject/index.vue
@@ -17,8 +17,7 @@ export default {
   },
   mounted() {
     PDFObject.embed(this.slice.primary.media.url, document.body, {
-      fallbackLink:
-        "<div class='relative w-screen h-screen overflow-hidden'><div class='absolute top-1/2 left-1/2 -transform-x-1/2 -transform-y-1/2 max-h-full w-full text-center'><p>Falls der Download nicht automatisch beginnt bitte <a class='link' href='[url]'>hier klicken</a>.</p></div></div>",
+      fallbackLink: `<div class='relative w-full h-screen overflow-hidden flex items-center justify-center'><div class='container p-4 mx-auto text-center'><a href="/" class="flex items-center justify-start space-x-2"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 17l-5-5m0 0l5-5m-5 5h12" /></svg><span class="block">Homepage</span></a><h1 class="title mb-2 mt-4">${this.slice.primary.media.name}</h1><p>Falls der Download nicht automatisch beginnt bitte <a class='link' href='[url]'>hier klicken</a>.</p></div></div>`,
     })
     if (!PDFObject.supportsPDFs) {
       this.$axios
